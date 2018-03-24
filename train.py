fae89885c9a9b4445425da40dd9ff7fa7cde44c6
Emile Server <epapillon@mobis-usa.com>
2018-03-24 21:11:01 +0000

    commented out the training part to troubleshoot the loading of images

--- train.py
+++ train.py
@@ -8,7 +8,7 @@ from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
 
 lines = []
-with open('./data/driving_log.csv') as csvfile:
+with open('../data/driving_log.csv') as csvfile:
 	reader = csv.reader(csvfile)
 	has_header = csv.Sniffer().has_header(csvfile.read(1024))
 	csvfile.seek(0) #rewind
@@ -30,15 +30,17 @@ for line in lines:
 
 X_train = np.array(images)
 y_train = np.array(measurements)
-
+print(X_train.shape)
+print(y_train.shape)
 #model
+'''
 model = Sequential()
 model.add(Lambda(lambda x: (x-128)/128, input_shape = (160,320,3)))
-model.add(Convolution2D(6,5,5), activation='relu')
+model.add(Convolution2D(6,5,5, activation='relu'))
 model.add(MaxPooling2D())
-model.add(Convolution2D(6,5,5), activation='relu')
+model.add(Convolution2D(6,5,5, activation='relu'))
 model.add(MaxPooling2D())
-model.add(Convolution2D(6,5,5), activation='relu')
+model.add(Convolution2D(6,5,5, activation='relu'))
 model.add(MaxPooling2D())
 model.add(Dense(120))
 model.add(Dense(84))
@@ -49,3 +51,4 @@ model.compile(loss='mse', optimizer='adam')
 model.fit(X_train, y_train, validation_split= 0.3, shuffle=True, nb_epoch = 5 )
 
 model.save('model.h5')
+'''
