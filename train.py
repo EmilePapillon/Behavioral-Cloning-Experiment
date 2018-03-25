1f4066d914a20e842729d73cb79ed941f861ec98
Emile Papillon <Papillon@Emile.local>
2018-03-24 21:55:34 -0400

    fixed the data loading problem, the path was set wrong

--- train.py
+++ train.py
@@ -8,12 +8,12 @@ from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
 
 lines = []
-with open('../data/driving_log.csv') as csvfile:
+with open('../data/driving_log.csv', 'r', newline='') as csvfile:
 	reader = csv.reader(csvfile)
-	has_header = csv.Sniffer().has_header(csvfile.read(1024))
-	csvfile.seek(0) #rewind
-	if has_header:
-		next(reader)
+	#has_header = csv.Sniffer().has_header(csvfile.read(1024))
+	#csvfile.seek(0) #rewind
+	#if has_header:
+	#	next(reader)
 	for line in reader:
 		lines.append(line)
 
@@ -22,7 +22,7 @@ measurements = []
 for line in lines:
 	source_path = line[0]
 	filename = source_path.split('/')[-1]
-	current_path = './data/IMG/' + filename
+	current_path = '../data/IMG/' + filename
 	image = cv2.imread(current_path)
 	images.append(image)
 	measurement = float(line[3])
@@ -30,16 +30,16 @@ for line in lines:
 
 X_train = np.array(images)
 y_train = np.array(measurements)
-print(X_train.shape)
-print(y_train.shape)
+#print(X_train.shape)
+#print(y_train.shape)
 #model
-'''
+
 model = Sequential()
 model.add(Lambda(lambda x: (x-128)/128, input_shape = (160,320,3)))
 model.add(Convolution2D(6,5,5, activation='relu'))
-model.add(MaxPooling2D())
+#model.add(MaxPooling2D())
 model.add(Convolution2D(6,5,5, activation='relu'))
-model.add(MaxPooling2D())
+#model.add(MaxPooling2D())
 model.add(Convolution2D(6,5,5, activation='relu'))
 model.add(MaxPooling2D())
 model.add(Dense(120))
@@ -51,4 +51,4 @@ model.compile(loss='mse', optimizer='adam')
 model.fit(X_train, y_train, validation_split= 0.3, shuffle=True, nb_epoch = 5 )
 
 model.save('model.h5')
-'''
+
