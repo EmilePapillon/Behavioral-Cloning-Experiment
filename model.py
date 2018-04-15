3a509f21aad9c703f34c733e9f2b9b6801a2a0fe
Emile Papillon <Papillon@Emile.local>
2018-04-15 19:36:29 -0400

    added a random offsetting function

--- model.py
+++ model.py
@@ -20,24 +20,55 @@ from math import floor
 from itertools import tee
 
 # Data augmentation functions definitions
-def do_nothing(image,angle):
-    return image, angle
+# Data augmentation functions definitions
+def crop_img(image,crop_factor,offset=[0,0]):
+    cf= crop_factor
+    h,w,ch = image.shape
+    if h%cf is not 0 or w%cf is not 0 : 
+        raise(ValueError("crop factor should be a common divider of the height and the width")) 
+    #print(h)
+    #print(w)
+    x1= int(w*(0.5-1/(2*cf)) + offset[0])
+    x2= int(w*(0.5+1/(2*cf)) + offset[0])
+    y1= int(h*(0.5-1/(2*cf)) + offset[1])
+    y2= int(h*(0.5+1/(2*cf)) + offset[1])
+    #print(x1,x2,y1,y2)
+    return image[y1:y2, x1:x2]
 
-def grayscale(image, angle):
+def random_offset(image,crop_factor,angle):
+    
+    h,w,ch = image.shape
+    max_offset_x = 0.5*w*(1-1/crop_factor)
+    max_offset_y = 0.5*h*(1-1/crop_factor)
+    
+    
+    offset_x = np.random.randint(-max_offset_x, max_offset_x)
+    offset_y = np.random.randint(-max_offset_y, max_offset_y)
+    return crop_img(image,cf,[offset_x,offset_y]), angle - (offset_x* (4/w) * 1.2/10)
+    
+def do_nothing(image,crop_factor,angle):
+    cf= crop_factor
+    return crop_img(image,cf), angle
+
+def grayscale(image, crop_factor ,angle):
+    cf=crop_factor
     gray = cv2.cvtColor(np.copy(image), cv2.COLOR_RGB2GRAY)
-    return cv2.cvtColor(gray, cv2.COLOR_GRAY2RGB), angle
+    return crop_img(cv2.cvtColor(gray, cv2.COLOR_GRAY2RGB),cf), angle
 
-def mirror(image, angle): 
-    return np.fliplr(np.copy(image)), -angle
+def mirror(image,crop_factor, angle): 
+    cf=crop_factor
+    return crop_img(np.fliplr(np.copy(image)),cf), -angle
 
-def random_brightness(image, angle):
+def random_brightness(image, crop_factor,angle):
+    cf=crop_factor
     image1 = cv2.cvtColor(np.copy(image),cv2.COLOR_RGB2HSV)
     random_bright = 0.8 + 0.4*(2*np.random.uniform()-1.0)    
     image1[:,:,2] = image1[:,:,2]*random_bright
     image1 = cv2.cvtColor(image1,cv2.COLOR_HSV2RGB)
-    return image1, angle
+    return crop_img(image1,cf), angle
 
-def random_translation(img, angle):
+def random_translation(img, crop_factor, angle):
+    cf=crop_factor
     tx_range,ty_range = 64,64
     rows,cols,ch = img.shape
     p=10 #pad the image (reflect the boundary)
@@ -48,9 +79,8 @@ def random_translation(img, angle):
     wrap = cv2.copyMakeBorder(img,p,p,p,p,0) #pads image with 10px uniform border
     img = cv2.warpAffine(wrap,Trans_M,(cols,rows))
     #img = img[p:p+cols,p:p+rows]
-    
-    new_angle = angle + tr_x * 3.75e-4
-    return img, new_angle 
+    new_angle=  angle + 3.75e-4 * tr_x
+    return crop_img(img,cf), angle #how to calculate angle??
 
 #return a list of tuples, each tuple containing path and angle for a training image, with the list of methods appended
 def make_reference_list(methods_index, data, offset=0.2):
@@ -63,7 +93,9 @@ def pick(numpy_array,index):
     return numpy_array[index], np.delete(numpy_array, index)
 
 # the logic for making batches
-def batch_generator(training_data_reference, methods_list, batch_size = 32):
+def batch_generator(training_data_reference, methods_list,crop_factor ,batch_size = 32):
+    
+    cf=crop_factor
     #define a list of transformations for each image in the data
     restart_flag = False
     while True:
@@ -83,7 +115,7 @@ def batch_generator(training_data_reference, methods_list, batch_size = 32):
 
                 # get the next batch images
                 batch_samples = training_data_reference[offset:offset+batch_size]
-                X_batch, y_batch = np.empty((batch_size, 160 ,320, 3), dtype = np.uint8), np.empty(batch_size)
+                X_batch, y_batch = np.empty((batch_size, int(160/cf) ,int(320/cf), 3), dtype = np.uint8), np.empty(batch_size)
 
                 #for each image in batch...
                 for idx in range(len(batch_samples)):                 
@@ -104,7 +136,7 @@ def batch_generator(training_data_reference, methods_list, batch_size = 32):
                         restart_flag = True
                         break
                     # get image and angle from calling selected method
-                    X_batch[idx], y_batch[idx] = method(image,angle)
+                    X_batch[idx], y_batch[idx] = method(image,cf,angle)
 
                 if restart_flag: 
                     break
@@ -119,7 +151,7 @@ data_file = '../data/driving_log.csv'
 data = pd.read_csv(data_file, header= None, names = ['center', 'left', 'right', 'steering_angle', 'x','y','z'])
 
 #list all the possible augmentation methods here
-methods = [grayscale, mirror, random_brightness,do_nothing]
+methods = [grayscale, mirror, random_brightness,random_offset,do_nothing]
 #methods = [grayscale, mirror, random_brightness,random_translation,do_nothing]
 
 methods_index = np.array(range(len(methods)))
@@ -130,11 +162,12 @@ training_data_reference = shuffle(make_reference_list(methods_index,data,offset=
 train_samples, validation_samples = train_test_split(training_data_reference, test_size= 0.2)
 
 bs = 32
-train_generator,train_generator_copy  = tee(batch_generator(train_samples , methods ,batch_size=bs))
-validation_generator = batch_generator(validation_samples ,methods ,  batch_size=bs)
+cf=1.6
+train_generator,train_generator_copy  = tee(batch_generator(train_samples , methods , cf,batch_size=bs))
+validation_generator = batch_generator(validation_samples ,methods ,cf,  batch_size=bs)
 
 X_sample,_ = next(train_generator_copy)
-row, col, ch =  X_sample.shape
+row, col, ch =  X_sample[0].shape
  
 #model
 model = Sequential()
@@ -177,3 +210,5 @@ plt.xlabel('epoch')
 plt.legend(['training set', 'validation set'], loc='upper right')
 #plt.show()
 plt.savefig('../loss.png')
+
+
